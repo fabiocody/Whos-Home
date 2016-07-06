bf29d8a57a9c70d65b3593cd471b8f432be883b9
fabiocody <fabiocody@icloud.com>
2016-07-06 21:52:47 +0200

    Add some remarks

--- whos@home.py
+++ whos@home.py
@@ -30,6 +30,7 @@ except:
 	print(colors.RED + 'ERROR opening people.json' + colors.END)
 	exit()
 
+# Make people list
 max_cycles = 30
 people = []
 for person_dict in people_json['people']:
@@ -41,20 +42,19 @@ for person_dict in people:
 def execute_process(bash_command):
     return subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
 
+# Main cycle,
 arp_command = 'sudo arp-scan --interface ' + interface + ' --localnet'
 while True:
 	output = execute_process(arp_command)
-	file = open(script_path + 'people.txt', 'w')
+	file = open(script_path + 'people.txt', 'w')  # Write output to a txt file to make presence data available to other programs
 	for line in output.stdout.readlines():
 		line = line.decode('utf8')
 		for split in line.split():
-			if len(split) == 17:
-				mac =  split[9:]
-				#print(mac)
+			if len(split) == 17:    # A MAC address is 17 characters long
+				mac =  split[9:]    # Only the last 3 bytes of the MAC address are taken into account, to ensure compatibility with some network devices which may change the vendor part of MAC addresses
 				for person in people:
-					#print(person['target'])
 					if mac == person['target']:
-						person['lastSeen'] = -1
+						person['lastSeen'] = -1    # The counter is set to -1 because every counter will be incremented in the next for cycle
 	for person in people:
 		if person['lastSeen'] < max_cycles:
 			person['lastSeen'] += 1
