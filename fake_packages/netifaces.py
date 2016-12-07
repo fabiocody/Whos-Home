e2f8c031eb71d1f624bef35a99d27bd73c5fe1a3
fabiocody <fabiocody@icloud.com>
2016-12-07 12:07:56 +0100

    Add fake_packages for travis-ci

---
+++ fake_packages/netifaces.py
@@ -0,0 +1,15 @@
+"""A fake minimal version of netifaces module.
+This module should only be used to work around a limitation of the travis-ci
+test environment.
+"""
+
+AF_INET = 2
+AF_LINK = 17
+
+
+def interfaces(self):  # pragma: no cover
+    return ['lo', 'eth0']
+
+
+def ifaddresses(self, iface):  # pragma: no cover
+    return {AF_LINK: [{'addr': '00:11:22:33:44:55'}]}
