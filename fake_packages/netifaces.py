338c75447b3142db46e55d1d36dfb28b2c910534
Fabio Codiglioni <fabiocody@icloud.com>
2018-02-03 15:34:14 +0100

    Update fake package

--- fake_packages/netifaces.py
+++ fake_packages/netifaces.py
@@ -1,4 +1,5 @@
-"""A fake minimal version of netifaces module.
+"""
+A fake minimal version of netifaces module.
 This module should only be used to work around a limitation of the travis-ci
 test environment.
 """
@@ -8,8 +9,8 @@ AF_LINK = 17
 
 
 def interfaces(self):  # pragma: no cover
-    return ['lo', 'eth0']
+	return ['lo', 'eth0']
 
 
 def ifaddresses(self, iface):  # pragma: no cover
-    return {AF_LINK: [{'addr': '00:11:22:33:44:55'}]}
+	return {AF_LINK: [{'addr': '00:11:22:33:44:55'}]}
